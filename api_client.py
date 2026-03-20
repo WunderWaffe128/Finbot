62c258257846d5b26ad7010c043a34f8b3386e53
Valentin <gpt942761@gmail.com>
2026-03-20 09:56:42 +0300

    Добавление другой валюты

--- api_client.py
+++ api_client.py
@@ -1,12 +1,7 @@
-# api_client.py
 import requests
 
 
 def get_currency_rate(currency="USD"):
-    """
-    Получение курса валюты от Беларусбанка
-    currency: USD или RUB
-    """
     try:
         url = "https://belarusbank.by/api/kursExchange"
         response = requests.get(url)
@@ -21,6 +16,10 @@ def get_currency_rate(currency="USD"):
                 rate = float(bank["USD_out"])
             elif currency == "RUB":
                 rate = float(bank["RUB_out"])
+            elif currency == "EUR":
+                rate = float(bank["EUR_out"])
+            elif currency == "CNY":
+                rate = float(bank["CNY_out"])
             else:
                 raise ValueError(f"Неподдерживаемая валюта: {currency}")
 
@@ -35,10 +34,16 @@ def get_currency_rate(currency="USD"):
 
 
 def get_usd_rate():
-    """Получение курса USD"""
     return get_currency_rate("USD")
 
-
 def get_rub_rate():
-    """Получение курса RUB"""
     return get_currency_rate("RUB")
+
+def get_euro_rate():
+    return get_currency_rate("EUR")
+
+def get_cny_rate():
+    return get_currency_rate("CNY")
+
+
+
